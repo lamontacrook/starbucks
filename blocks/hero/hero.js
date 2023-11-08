59da68ef658d16d842d0d454ee8b2463d5836df1
Lamont Crook <lamont@adobe.com>
2023-11-08 11:46:53 -0700

    update

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,21 +1,33 @@
-export default function decorate(block) {
-  const anchor = block.querySelector('a');
-  const [topCallout, videoDiv, bottomCallout] = [...block.children];
+export default function decorate(element) {
+  const picture = element.querySelector('picture');
+  const [topCallout, assetDiv, bottomCallout] = [...element.children];
+  const { alt } = picture.querySelector('img');
 
   bottomCallout.classList.add('bottom-callout');
   topCallout.classList.add('top-callout');
-  videoDiv.classList.add('video');
-  videoDiv.innerHTML = `<video loop muted playsInline>
-      <source data-src="${anchor?.href}" type="video/mp4" />
-    </video>`;
-  const video = block.querySelector('video');
-  const source = block.querySelector('video > source');
+  assetDiv.classList.add('asset');
+  if (element.classList.contains('video')) {
+    const configPath = `${window.location.origin}/videos.json`;
+    fetch(configPath).then((res) => {
+      res.json().then((json) => {
+        json?.data.forEach(({ block, video, poster }) => {
+          if (block === 'hero' && alt.includes(poster)) {
+            assetDiv.innerHTML = `<video loop muted playsInline>
+                <source data-src="${video}" type="video/mp4" />
+              </video>`;
+            const vdo = assetDiv.querySelector('video');
+            const source = assetDiv.querySelector('video > source');
 
-  source.src = source.dataset.src;
-  video.load();
-  video.addEventListener('loadeddata', () => {
-    video.setAttribute('autoplay', true);
-    video.setAttribute('data-loaded', true);
-    video.play();
-  });
+            source.src = source.dataset.src;
+            vdo.load();
+            vdo.addEventListener('loadeddata', () => {
+              vdo.setAttribute('autoplay', true);
+              vdo.setAttribute('data-loaded', true);
+              vdo.play();
+            });
+          }
+        });
+      });
+    });
+  }
 }
